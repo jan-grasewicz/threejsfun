a277d8e39307db2886959410da0a1da2224a3ca0
jan-grasewicz <45798702+jan-grasewicz@users.noreply.github.com>
2020-12-28 00:34:22 +0100

    wip animating box transformation

--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Canvas } from 'react-three-fiber'
-import Box from '../Box'
-import GroupMesh from '../GroupMesh'
+import TransformingBox from '../TransformingBox'
+// import Box from '../Box'
+// import GroupMesh from '../GroupMesh'
 
 const Root = () => {
-	return (
-		<Canvas>
-			<ambientLight />
-			<pointLight position={[10, 10, 10]} />
-			<Box position={[-2, 0, 1]} />
-			<GroupMesh position={[3, 0, -5]} />
-		</Canvas>
-	)
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <TransformingBox position={[0, -1, 0]} />
+      {/* <Box position={[-4, -1, 1]} /> */}
+      {/* <GroupMesh position={[3, 0, -5]} /> */}
+    </Canvas>
+  )
 }
 
 export default Root
---
+++ src/components/TransformingBox.js
@@ -0,0 +1,89 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import { Spring } from 'react-spring/renderprops'
+import { a, useSpring } from 'react-spring/three'
+import { useFrame, useUpdate } from 'react-three-fiber'
+import * as THREE from 'three'
+
+const BoxQuarter = ({ height = 3, spikeXPos = 1, ...rest }) => {
+  const meshRef = useUpdate(
+    (props) => {
+      // console.log('tst', props)
+      const { geometry } = props
+      geometry.vertices[0].x = spikeXPos
+      // geometry.verticesNeedUpdate = true
+      geometry.elementsNeedUpdate = true
+      geometry.computeBoundingSphere()
+      geometry.computeFaceNormals()
+      // geometry.normalize()
+    },
+    [spikeXPos]
+  )
+
+  // useFrame(() => {
+  //   meshRef.current.rotation.y += 0.01
+  // })
+
+  return (
+    <mesh {...rest} ref={meshRef}>
+      <geometry
+        attach='geometry'
+        // ref={tetraRef}
+        vertices={[
+          new THREE.Vector3(1, -height / 2, 1),
+          new THREE.Vector3(1, height / 2, 1),
+          new THREE.Vector3(-1, height / 2, 1),
+          new THREE.Vector3(1, height / 2, -1),
+        ]}
+        faces={[
+          new THREE.Face3(0, 1, 2),
+          new THREE.Face3(0, 3, 1),
+          new THREE.Face3(0, 2, 3),
+          new THREE.Face3(1, 3, 2),
+        ]}
+      />
+      <meshStandardMaterial attach='material' color={0xf95b3c} />
+    </mesh>
+  )
+}
+
+const TransformingBox = ({ ...rest }) => {
+  const [open, setOpen] = useState(false)
+  const { topRotation, spikeXPos } = useSpring({
+    spikeXPos: open ? -1 : 1,
+    topRotation: open ? Math.PI / 2 : 0,
+  })
+  // console.log(spring)
+  const boxRef = useRef()
+
+  useFrame(() => {
+    boxRef.current.rotation.y += 0.01
+  })
+  // const spikeXPos = open ? -1 : 1
+  const boxQuarterProps = { spikeXPos, height: 4 }
+  const handleClick = (e) => {
+    e.stopPropagation()
+    setOpen((prev) => !prev)
+  }
+
+  return (
+    <group ref={boxRef} onClick={handleClick} {...rest}>
+      <Spring to={{ spikeXPos: open ? -1 : 1 }}>
+        {(props) => (
+          <>
+            <a.group position-y={0.5} rotation-y={topRotation}>
+              <BoxQuarter spikeXPos={props.spikeXPos} />
+              {/* <BoxQuarter spikeXPos={props.spikeXPos} /> */}
+              <BoxQuarter spikeXPos={props.spikeXPos} rotation-y={Math.PI} />
+            </a.group>
+            <group rotation-x={Math.PI}>
+              <BoxQuarter spikeXPos={props.spikeXPos} />
+              <BoxQuarter spikeXPos={props.spikeXPos} rotation-y={Math.PI} />
+            </group>
+          </>
+        )}
+      </Spring>
+    </group>
+  )
+}
+
+export default TransformingBox
